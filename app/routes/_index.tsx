8e7d575c0c921ac7b9abc8fdc5f95d8601b22824
pdmuds4 <ryosuke3191@gmail.com>
2024-10-18 02:55:22 +0900

    home画面を実装

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,20 +1,33 @@
-import { Text, Box, Image, Stack } from "@chakra-ui/react";
+import { Text, Box, Image, Stack, Icon } from "@chakra-ui/react";
+import { MdOutlineLiveTv } from "react-icons/md";
 
 import { HomeLayout } from "~/src/components";
+import OriginalButton from "~/src/components/originalButton";
 
 export default function Index() {
     return (
         <HomeLayout>
             <Text fontSize='min(12vw, 32px)'>
-                Live Hunturn
+                Go Live !
             </Text>
-            <Box>
-                <Image />
-                
-
+            <Box className="text-center">
+                <Image 
+                    className="shadow-md"
+                    src='https://yt3.googleusercontent.com/ytc/AIdro_k-adfLoZx59Y62rNV58cop7fhtr4RK8K5YBORgORjDBVQ=s160-c-k-c0x00ffffff-no-rj'
+                    alt="youtube account avatar"
+                    minW={100}
+                    borderRadius='full'
+                />
+                <Text fontSize='min(8vw, 32px)'>
+                    PAM
+                </Text>
             </Box>
             <Stack>
-
+                <OriginalButton
+                    leftIcon={<Icon as={MdOutlineLiveTv} />}
+                >
+                    配信に接続する
+                </OriginalButton>
             </Stack>
         </HomeLayout>
     );
