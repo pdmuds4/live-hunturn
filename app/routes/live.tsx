e8b821afdaab3f2a72fb4f520314720416a17651
pdmuds4 <ryosuke3191@gmail.com>
2024-11-03 12:11:58 +0900

    /liveのAuthProviderを除去

--- app/routes/live.tsx
+++ app/routes/live.tsx
@@ -4,7 +4,7 @@ import apiHandler from "~/src/utils/apiHandler";
 import { useLoaderData } from "@remix-run/react";
 
 import { Card, Flex } from "@chakra-ui/react";
-import { ViewHunterRow, ProviderAuth, UiButton } from "~/src/components";
+import { ViewHunterRow, UiButton } from "~/src/components";
 
 import axios from "axios";
 import { YoutubeLiveApi } from "~/src/types";
@@ -24,42 +24,40 @@ export default function Live() {
     const { hunters, questDoneHandler } = useHuntersManager(data);
 
     return (
-        <ProviderAuth>
-            <Flex className="w-screen h-screen justify-center">
-                <Card className="relative w-full bg-slate-700/95" maxW='500px' minW="200px">
-                    <Card className="w-full border-yellow-700 border-4 border-double bg-slate-950/95 text-white" h={132}>
-                        <Flex direction='column'>
-                            <ViewHunterRow 
-                                {...hunters.host}
-                                status="join-us"
-                                quest={0}
-                                is_owner
-                            />
-                            { hunters.Joined.map((joiner, index) => (
-                                <ViewHunterRow 
-                                    key={index}
-                                    {...joiner}
-                                    status="join-us"
-                                    is_owner={false}
-                                />
-                            ))}
-                        </Flex>
-                    </Card>
-                    <Flex className="text-white p-1 h-full" direction='column'>
-                        <UiButton onClick={questDoneHandler}>
-                            クエスト終了
-                        </UiButton>
-                        { hunters.StandBy.map((standby, index) =>  (
+        <Flex className="w-screen h-screen justify-center">
+            <Card className="relative w-full bg-slate-700/95" maxW='500px' minW="200px">
+                <Card className="w-full border-yellow-700 border-4 border-double bg-slate-950/95 text-white" h={132}>
+                    <Flex direction='column'>
+                        <ViewHunterRow 
+                            {...hunters.host}
+                            status="join-us"
+                            quest={0}
+                            is_owner
+                        />
+                        { hunters.Joined.map((joiner, index) => (
                             <ViewHunterRow 
                                 key={index}
-                                {...standby}
-                                status="stand-by"
+                                {...joiner}
+                                status="join-us"
                                 is_owner={false}
                             />
                         ))}
                     </Flex>
                 </Card>
-            </Flex>
-        </ProviderAuth>
+                <Flex className="text-white p-1 h-full" direction='column'>
+                    <UiButton onClick={questDoneHandler}>
+                        クエスト終了
+                    </UiButton>
+                    { hunters.StandBy.map((standby, index) =>  (
+                        <ViewHunterRow 
+                            key={index}
+                            {...standby}
+                            status="stand-by"
+                            is_owner={false}
+                        />
+                    ))}
+                </Flex>
+            </Card>
+        </Flex>
     );
 }
