610b004aebcb04391c609c3018d5f83b16956c1a
pdmuds4 <ryosuke3191@gmail.com>
2024-11-10 15:26:25 +0900

    /api/live_chatを実装

--- app/routes/api.live-chat.ts
+++ app/routes/api.live-chat.ts
@@ -5,6 +5,11 @@ import ServerError from "~/src/utils/serverError";
 import { LiveChatApi } from "~/src/types";
 import puppeteer from "puppeteer";
 
+import CommandProcessor from "~/src/client/command";
+import command_parser from "~/src/command";
+
+const command = new CommandProcessor(command_parser);
+
 export const loader = (args: LoaderFunctionArgs) => apiHandler(
     args,
     async ({ request }) => {
@@ -12,18 +17,51 @@ export const loader = (args: LoaderFunctionArgs) => apiHandler(
         const liveID = param.get('live_id');
 
         if (liveID) {
-            const browser = await puppeteer.launch({headless: false});
-            const page = await browser.newPage();
-            await page.goto(`https://www.youtube.com/live_chat?is_popout=1&v=${liveID}`);
-            await page.waitForSelector('yt-live-chat-text-message-renderer');
-
-            const messages = await page.$$eval('yt-live-chat-text-message-renderer', (elements) => {
-                return elements.map((element) => {
-                    return element
-                });
+            const browser = await puppeteer.launch({
+                headless: false,
+                args: [
+                    '--window-position=-1000,-1000',  // 画面外に配置
+                    '--disable-gpu', // GPU使用を無効にする
+                    '--disable-software-rasterizer'  // ソフトウェアレンダリングを無効にする
+                ]
             });
-            return json({ messages });
-            
+            const page = await browser.newPage();
+
+            try {
+                await page.goto(`https://www.youtube.com/live_chat?is_popout=1&v=${liveID}`, { waitUntil: 'networkidle2' });
+                await page.waitForSelector('yt-live-chat-text-message-renderer');
+                
+                const chat_infos = await page.$$eval('yt-live-chat-text-message-renderer', (elements) => {
+                    return elements.map((element) => {
+                        const message = element.querySelector('#message')?.textContent;
+                        if (message) {
+                            const avator = element.querySelector('.yt-img-shadow')!.getAttribute('src');
+                            return {
+                                id: avator?.replace('https://yt3.ggpht.com', '').split('=')[0] || '',
+                                name: element.querySelector('#author-name')!.textContent || '',
+                                avator: avator || '',
+                                message: message,
+                                timestamp: new Date()
+                            }
+                        } else {
+                            return null;
+                        }
+                    }).filter((command) => command !== null);
+                });
+
+                const query = chat_infos.map((user_info) => {
+                    if (command.recognizer(user_info.message)) {
+                        const request = command.toRequest(user_info.message);
+                        return { request, user_info }
+                    } else {
+                        return null;
+                    }
+                }).filter((command) => command !== null);
+
+                return json({ query: query.length ? query: null }) as TypedResponse<LiveChatApi.GETresponse>;
+            } finally {
+                await browser.close();
+            }
         } else {
             throw new ServerError(
                 '配信IDを入力してください',
